6af6d1875e061afa92c570ded545b87fd447ac70
santidotpy <santidotpy@gmail.com>
2024-05-16 21:27:55 -0300

    ✨ update address to use with Docker

--- chat/server.py
+++ chat/server.py
@@ -19,7 +19,8 @@ messages = [] # Lista de mensajes de chat
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Chat server")
-    parser.add_argument("host", nargs='?', type=str, help="Server host", default="127.0.0.1")
+    # parser.add_argument("host", nargs='?', type=str, help="Server host", default="127.0.0.1") # LOCALHOST
+    parser.add_argument("host", nargs='?', type=str, help="Server host", default="0.0.0.0")
     parser.add_argument("port", nargs='?', type=int, help="Server port", default=55555)
     parser.add_argument("usedb", nargs='?', type=bool, help="Bool to use db", default=True)
     return parser.parse_args()
@@ -43,32 +44,15 @@ def get_messages_data(author, messages):
     only_messages = [get_author_and_message(message.decode('utf-8'))[1] for message in messages]
     data = {
         "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        # "messages": [get_message_data(message) for message in messages],
-        # "messages":[ message.decode('utf-8') for message in messages],
         "messages": only_messages,
         "author": author
     }
     return data
 
-    # return [get_message_data(message) for message in messages]
-
-# def otraManeraDeLoDeArriba(author, messages):
-#     data = []
-#     data.append({
-#         "author": author,
-#         "messages": messages,
-#         "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#     })
-#     return data
 
 def save_messages(author, messages):
     doc_ref = db.collection("chatCollection").document()
     doc_ref.set(get_messages_data(author, messages))
-
-
-def save_message(message):
-    doc_ref = db.collection("chatCollection").document()
-    doc_ref.set(get_message_data(message))
 
 
 # Función para transmitir mensajes a todos los clientes
@@ -88,24 +72,18 @@ def handle_client(client):
             # Transmitir mensaje a todos los clientes
             broadcast(message)
             print(only_message)
-            print(only_message == 'exit')
-            if only_message == 'EXIT':
-                raise Exception("Cliente desconectado.")
+            print(only_message == 'EXIT')
+            if only_message == 'EXIT': raise Exception("Cliente desconectado.")
         except:
             # Eliminar y cerrar la conexión con el cliente
             index = clients.index(client)
             clients.remove(client)
             client.close()
             username = usernames[index]
-            print(f"Mensajes enviados por {username} eliminados. {messages}")
+            # print(f"Mensajes enviados por {username} eliminados. {messages}")
             broadcast(f'{username} ha abandonado el chat.'.encode('utf-8'))
             usernames.remove(username)
-
-            if only_message == 'EXIT':
-                if use_db: save_messages(author, messages)
-                broadcast(b'Server is shutting down.')
-                server.close()
-                sys.exit("Server closed by admin.")
+            if use_db: save_messages(author, messages)
             break
 
 
